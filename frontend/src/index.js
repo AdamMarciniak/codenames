409768878ed261b581d7b328ab04c4fa29261948
Adam Marciniak <adam668@gmail.com>
2020-04-24 19:27:51 -0700

    Video fix again?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,9 +16,7 @@ const AntiIframeComponent = () => (
   <h2>
     К сожалению, этот сайт украл нашу игру, не спросив нас, поэтому мы не можем позволить вам увидеть ее. Хорошего дня.
   </h2>
-  <iframe width="420" height="315"
-src="https://www.youtube.com/embed?v=vWgjqMyP5kk?autoplay=1">
-</iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/Ca3uJOAObRo?controls=0&amp;start=133" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </>
 )
 
