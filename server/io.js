c3419cc5df965f234ab95c1dbd68ed9f6082e6e1
Adam Marciniak <adam668@gmail.com>
2020-06-27 23:00:53 -0700

    Update io.js

--- server/io.js
+++ server/io.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const cors = require("cors");
-const port = 6970;
+const port = 6971;
 
 setTimeout(() => http.listen(port, () => console.log(`Example app listening on port ${port}!`)));
 
