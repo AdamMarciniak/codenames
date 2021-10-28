463f5b1650e4a966b2a5322e3fb78574167b4e22
Adam Marciniak <17841889+AdamMarciniak@users.noreply.github.com>
2021-10-27 22:21:55 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 app.get(
   ['/new', '/join/:code', '/god', '/join', '/game/*', '/gallery'],
   (req, res) => {
+      console.log('GET');
       res.sendFile('/var/www/codenames/frontend/buildProduction/index.html')
     }
 )
