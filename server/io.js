f5b223ddf343c69cdbbca97b14c7da8759f0372a
Adam Marciniak <17841889+AdamMarciniak@users.noreply.github.com>
2021-10-27 22:11:44 -0700

    Update io.js

--- server/io.js
+++ server/io.js
@@ -2,8 +2,8 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const cors = require("cors");
-const port = 6968;
+const port = 6970;
 
-setTimeout(() => http.listen(port, () => console.log(`Example app listening on port ${port}!`)));
+setTimeout(() => http.listen(port, () => console.log(`IO websocket listening on port ${port}!`)));
 
 module.exports = io;
